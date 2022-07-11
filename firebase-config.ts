177807e1a3f595c753a6cd457c015d0808683f2b
Bobomurod <bobomurod.boboev@yandex.ru>
2022-07-11 20:05:21 +0300

    not using firebase

---
+++ firebase-config.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCtJnXTowuJXw7WA-W_WCGybM41lyqRwos',
+  authDomain: 'typescript-react-firebas-7a269.firebaseapp.com',
+  projectId: 'typescript-react-firebas-7a269',
+  storageBucket: 'typescript-react-firebas-7a269.appspot.com',
+  messagingSenderId: '22713145265',
+  appId: '1:22713145265:web:559d50edefd591440b4f90',
+};
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,75 @@
+/* eslint-disable import/order */
+/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  // useNavigate,
+} from 'react-router-dom';
+import { app } from '../firebase-config';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
 import Form from './Components/Common/Form';
+import Home from './Components/Home';
 import './App.css';
 
 function App() {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
-  const handleAction = (r) => {
-    console.log(email, password, r);
+  // const navigate = useNavigate();
+  const handleAction = (id) => {
+    const authentication = getAuth();
+    if (id === 'sign-up') {
+      createUserWithEmailAndPassword(authentication, email, password)
+        .then((response) => {
+          console.log(response);
+        // navigate('/home')
+        // sessionStorage.setItem('Auth Token', response._tokenResponse.refreshToken);
+        });
+    }
+    if (id === 'sign-in') {
+      signInWithEmailAndPassword(authentication, email, password)
+        .then((response) => {
+          console.log(response);
+        // navigate('/home')
+        // sessionStorage.setItem('Auth Token', response._tokenResponse.refreshToken);
+        });
+    }
   };
   return (
-    <Router>
-      <div className="App">
-        router
-        <Routes>
-          <Route
-            path="/login"
-            element={(
-              <Form
-                title="login"
-                setEmail={setEmail}
-                setPassword={setPassword}
-                handleAction={() => handleAction(1)}
-              />
+
+    <div className="App">
+      router
+      <Routes>
+        <Route
+          path="/login"
+          element={(
+            <Form
+              title="login"
+              setEmail={setEmail}
+              setPassword={setPassword}
+              handleAction={() => handleAction(1)}
+            />
             )}
-          />
-          <Route
-            path="/register"
-            element={(
-              <Form
-                title="register"
-                setEmail={setEmail}
-                setPassword={setPassword}
-                handleAction={() => handleAction(2)}
-              />
+        />
+        <Route
+          path="/register"
+          element={(
+            <Form
+              title="register"
+              setEmail={setEmail}
+              setPassword={setPassword}
+              handleAction={() => handleAction(2)}
+            />
             )}
-          />
-        </Routes>
-      </div>
-    </Router>
+        />
+        <Route
+          path="/home"
+          element={<Home />}
+        />
+      </Routes>
+    </div>
+
   );
 }
 
