160d6814cb697ff0b4bbf47465bf102bf501a916
Bobomurod <bobomurod.boboev@yandex.ru>
2022-07-13 01:16:49 +0300

    prepare to verify user by email

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,61 @@
-/* eslint-disable import/order */
-/* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
-  BrowserRouter as Router,
   Routes,
   Route,
-  // useNavigate,
+  useNavigate,
 } from 'react-router-dom';
 import { initializeApp } from 'firebase/app';
+import {
+  getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, UserCredential,
+} from 'firebase/auth';
 import { config } from './config';
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
 import Form from './Components/Common/Form';
-import Home from './Components/Home';
+import Home from './Pages/Home';
 import './App.css';
+import Unverified from './Pages/Unverified';
 
 initializeApp(config.firebaseConfig);
 
 function App() {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
-  // const navigate = useNavigate();
-  const handleAction = (id) => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const authToken = sessionStorage.getItem('AuthToken');
+    console.log(authToken);
+    if (authToken) {
+      navigate('/');
+    }
+  }, []);
+  const handleAction = async (id) => {
     const authentication = getAuth();
     if (id === 'sign-up') {
-      createUserWithEmailAndPassword(authentication, email, password)
-        .then((response) => {
+      await createUserWithEmailAndPassword(authentication, email, password)
+        .then(async (response) => {
           console.log(response);
-        // navigate('/home')
-        // sessionStorage.setItem('Auth Token', response._tokenResponse.refreshToken);
+          navigate('/unverified');
+          const authToken = await response.user.getIdToken();
+          sessionStorage.setItem('authToken', authToken);
+          sessionStorage.setItem('refreshToken', response.user.refreshToken);
+        })
+        .catch((error) => {
+          console.log(error);
         });
     }
     if (id === 'sign-in') {
-      signInWithEmailAndPassword(authentication, email, password)
-        .then((response) => {
+      await signInWithEmailAndPassword(authentication, email, password)
+        .then(async (response: UserCredential) => {
           console.log(response);
-        // navigate('/home')
-        // sessionStorage.setItem('Auth Token', response._tokenResponse.refreshToken);
+          const jwt = await response.user.getIdToken();
+          sessionStorage.setItem('authToken', jwt);
+          if (response.user.emailVerified) {
+            navigate('/');
+          } else {
+            navigate('/unverified');
+          }
+        })
+        .catch((error) => {
+          console.log(error);
         });
     }
   };
@@ -67,8 +87,12 @@ function App() {
             )}
         />
         <Route
-          path="/home"
+          path="/"
           element={<Home />}
+        />
+        <Route
+          path="/unverified"
+          element={<Unverified />}
         />
       </Routes>
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from '../Components/Common/Button';
 
 export default function Home() {
-  (
+  const navigate = useNavigate();
+  const handleAction = async () => {
+    sessionStorage.removeItem('authToken');
+    await navigate('/login');
+  };
+  useEffect(() => {
+    const authToken = sessionStorage.getItem('authToken');
+    if (authToken) {
+      navigate('/');
+    }
+    if (!authToken) {
+      navigate('/login');
+    }
+  }, []);
+  return (
     <div>
       <h1>Home</h1>
+      <Button title="Logout" handleAction={handleAction} />
     </div>
   );
 }
